99b58c59825171913fe105c247b35ec5eaf9485c
Mirek Simek <miroslav.simek@vscht.cz>
2019-11-15 11:14:45 +0100

    working config both for bili and vue-cli

--- bili.config.js
+++ bili.config.js
@@ -1,13 +1,17 @@
+const glob = require('glob');
+
+const input = glob('./*/*.vue', {sync: true}).filter(
+    (path) => {
+        const COMPONENT_PATH = /\.\/(\w*)\/(\w*)\.vue$/;
+        const [any, folder, file] = COMPONENT_PATH.exec(path) || [];
+        return (any && folder === file);
+    },
+);
+
 module.exports = {
-  plugins: {
-    babel: {
-      plugins: [
-["@babel/transform-runtime", {
-    corejs: 2,
-  }]
-      ],
-      externalHelpers: true,
-      runtimeHelpers: true
-    }
-  }
-}
+    input,
+    banner: true,
+    filename: 'src/library.js',
+    formats: ['es'],
+    plugins: [],
+};
